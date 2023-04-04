e375c3c938837088c03f64b5dec22f40f0aaba72
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-04-04 14:55:37 +0200

    adjustamen

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,4 @@ const surname = prompt('scrivi il tuo cognome');
 
 const color = prompt('il tuo colore preferito');
 
-document.writeln("<h1>ciao " + name + surname + color + '23</h1>')
+document.writeln("<h1>password: " + name + surname + color + '23</h1>')
